ed8d834a6e20bf26314fc24571bc30c047ef03d3
= <=>
2020-05-18 23:58:34 -0300

    Complex slide transition implemented

--- lib/src/slide/slide.dart
+++ lib/src/slide/slide.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:transitions/src/abstraction/transition.dart';
 
 abstract class SimpleSlideTransition extends Transition {
-  SimpleSlideTransition(Widget child) : super(child);
+  const SimpleSlideTransition(Widget child) : super(child);
 
   Offset get enterBeginOffset;
   Offset get enterEndOffset;
@@ -27,6 +27,43 @@ abstract class SimpleSlideTransition extends Transition {
     return SlideTransition(
       position: slideTransition,
       child: child,
+    );
+  }
+}
+
+abstract class ComplexSlideTransition extends SimpleSlideTransition {
+  const ComplexSlideTransition(Widget child) : super(child);
+
+  Offset get popBeginOffset;
+  Offset get popEndOffset;
+
+  @override
+  Widget animate(
+    BuildContext context,
+    Animation<double> enterAnimation,
+    Animation<double> popAnimation,
+    Widget child,
+  ) {
+    final simpleAnimation = super.animate(
+      context,
+      enterAnimation,
+      popAnimation,
+      child,
+    );
+
+    final slideTransition = Tween<Offset>(
+      begin: popBeginOffset,
+      end: popEndOffset,
+    ).animate(
+      CurvedAnimation(
+        curve: curve,
+        parent: popAnimation,
+      ),
+    );
+
+    return SlideTransition(
+      position: slideTransition,
+      child: simpleAnimation,
     );
   }
 }
